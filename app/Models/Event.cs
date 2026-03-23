bdb3febb9abb2d9e487299cd96f6faab1c72fa1a
Charles Girard <charlesgirard@hotmail.com>
2026-03-23 16:02:58 -0400

    Ajoute classe Event

---
+++ app/Models/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace app.Models
+{
+    public class Event
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required DateTime Start { get; set; }
+        public required DateTime End { get; set; }
+        public required string Location { get; set; }
+    }
+}
