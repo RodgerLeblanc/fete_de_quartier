dc3da1046b6514459bb4b7623cc75dcc0ffda14b
Charles Girard <charlesgirard@hotmail.com>
2026-03-26 21:59:51 -0400

    Ajoute contrôleur d'évènements

---
+++ app/Controllers/EventsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using app.Models;
+using app.Data;
+
+namespace app.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly FeteDeQuartierContext _context;
+
+        public EventsController(FeteDeQuartierContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var events = await _context.Events.OrderBy(e => e.Start).Include(e => e.Location).ToListAsync();
+
+            return View(events);
+        }
+    }
+}
