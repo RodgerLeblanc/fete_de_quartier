c29d68930c5b92a0a40eb6a5adac1d4c7bb43105
Charles Girard <charlesgirard@hotmail.com>
2026-03-28 10:23:35 -0400

    Crée fichier TS avec fonction popup

---
+++ app/scripts/app.ts
@@ -0,0 +1,7 @@
+﻿function popup(id: number) {
+    const url = 'Events/Details/' + id.toString();
+    const name = 'Détails';
+    const options = 'height=500,width=400,status=yes,toolbar=no,scrollbars=yes';
+
+    window.open(url, name, options)
+}
